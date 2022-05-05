10b97cd0842c9d90b5f418cce86d55009054f90c
Viktor Persson <vph.persson@gmail.com>
2022-05-05 10:41:50 +0200

    Refactor; use `ecs_py` instead of own structures.

--- logging_script.py
+++ logging_script.py
@@ -1,4 +1,3 @@
-from dataclasses import asdict
 from typing import Optional
 from pathlib import Path
 from json import dumps as json_dumps
@@ -15,9 +14,9 @@ from user_agents import parse as user_agents_parse
 from user_agents.parsers import UserAgent
 from magic import from_buffer as magic_from_buffer
 
-from structures import LogEntry, HttpLogEntry, RequestLogEntry, ResponseLogEntry, BodyLogEntry, UrlLogEntry, \
-    ClientLogEntry, ServerLogEntry, UserAgentLogEntry, UserAgentOSLogEntry, UserAgentDeviceLogEntry, \
-    NetworkLogEntry, EventLogEntry
+from ecs_py import Base, Http, HttpRequest, HttpRequestBody, HttpResponse, HttpResponseBody, URL, Client, Server, \
+    Network, Event, UserAgent as ECSUserAgent, UserAgentDevice, OS
+from ecs_tools_py import event_timezone_from_datetime
 
 
 class WebProxyLogger:
@@ -27,12 +26,13 @@ class WebProxyLogger:
 
         self.public_suffix_list_trie = PublicSuffixListTrie.from_public_suffix_list_file(file=Path('public_suffix_list.dat'))
 
-    def _build_log_entry(self, flow: HTTPFlow) -> LogEntry:
+    def _build_log_entry(self, flow: HTTPFlow) -> Base:
 
         event_created: datetime = datetime.now().astimezone()
 
-        response_log_entry: Optional[ResponseLogEntry] = None
+        ecs_http_response: Optional[HttpResponse] = None
         event_end: Optional[datetime] = None
+
         if flow.response:
             response_raw_content: Optional[bytes] = flow.response.raw_content
             response_raw_headers = bytes(flow.response.headers)
@@ -42,8 +42,8 @@ class WebProxyLogger:
                 for key, value in flow.response.headers.items()
             }
 
-            response_log_entry = ResponseLogEntry(
-                body=BodyLogEntry(
+            ecs_http_response = HttpResponse(
+                body=HttpResponseBody(
                     bytes=(len(response_raw_content) if response_raw_content is not None else 0),
                     content=None
                 ),
@@ -57,7 +57,7 @@ class WebProxyLogger:
                     buffer=(response_raw_content or b''),
                     mime=True
                 ).lower() if response_raw_content else None,
-                mime_type_content_type=[
+                content_type_mime_type=[
                     re_sub(pattern=r'; charset=.+$', repl='', string=content_type)
                     for content_type in (response_headers.get('content_type') or [])
                 ]
@@ -108,101 +108,102 @@ class WebProxyLogger:
 
         domain_properties: DomainProperties = self.public_suffix_list_trie.get_domain_properties(domain=flow.request.host)
 
-        user_agent_log_entry: Optional[UserAgentLogEntry] = None
         if user_agent_string := flow.request.headers.get('user-agent'):
-
-            user_agent_log_entry = UserAgentLogEntry(original=user_agent_string)
+            ecs_user_agent = ECSUserAgent(original=user_agent_string)
 
             try:
                 user_agent: UserAgent = user_agents_parse(user_agent_string=user_agent_string)
             except:
                 pass
             else:
-                user_agent_log_entry.device = (
-                    UserAgentDeviceLogEntry(name=user_agent.device.family)
-                    if user_agent.device.family != 'Other' else None
-                )
-                user_agent_log_entry.name = user_agent.browser.family
-                user_agent_log_entry.os = UserAgentOSLogEntry(
+                ecs_user_agent.device = UserAgentDevice(
+                    name=user_agent.device.family
+                ) if user_agent.device.family != 'Other' else None
+                ecs_user_agent.name = user_agent.browser.family
+                ecs_user_agent.os = OS(
                     family=user_agent.os.family,
                     version=user_agent.os.version_string
                 )
-                user_agent_log_entry.version = user_agent.browser.version_string
+                ecs_user_agent.version = user_agent.browser.version_string
+        else:
+            ecs_user_agent = None
 
-        return LogEntry(
-            http=HttpLogEntry(
-                request=RequestLogEntry(
-                    bytes=(len(request_raw_headers) + (len(request_raw_content) if request_raw_content is not None else 0)),
-                    headers=request_headers or None,
-                    method=flow.request.method,
-                    mime_type=request_mime_type if request_raw_content else None,
-                    mime_type_content_type=[
-                        re_sub(pattern=r'; charset=.+$', repl='', string=content_type)
-                        for content_type in (request_headers.get('content_type') or [])
-                    ] or None,
-                    body=BodyLogEntry(
+        return Base(
+            http=Http(
+                request=HttpRequest(
+                    body=HttpRequestBody(
                         bytes=(len(request_raw_content) if request_raw_content is not None else 0),
                         content=(flow.request.get_text(strict=False) or None) if include_request_content else None,
                         decompressed_content=(
-                            decompressed_request_content.decode(encoding='utf-8', errors='surrogateescape') or None
+                                decompressed_request_content.decode(encoding='utf-8', errors='surrogateescape') or None
                         ) if include_decompressed_request_content else None
                     ),
+                    headers=request_headers or None,
+                    bytes=(
+                        len(request_raw_headers) + (len(request_raw_content) if request_raw_content is not None else 0)
+                    ),
+                    mime_type=request_mime_type if request_raw_content else None,
+                    content_type_mime_type=[
+                        re_sub(pattern=r'; charset=.+$', repl='', string=content_type)
+                        for content_type in (request_headers.get('content_type') or [])
+                    ] or None,
+                    method=flow.request.method,
                     referrer=flow.request.headers.get('referer')
                 ),
-                response=response_log_entry,
+                response=ecs_http_response,
                 version=flow.request.http_version.removeprefix('HTTP/')
             ),
-            url=UrlLogEntry(
+            url=URL(
                 domain=flow.request.host,
-                full=flow.request.url,
-                original=flow.request.url,
-                path=flow.request.path,
-                port=flow.request.port,
-                scheme=flow.request.scheme,
-                subdomain=domain_properties.subdomain,
-                top_level_domain=domain_properties.effective_top_level_domain,
                 extension=(
                     (
                         Path(re_sub(pattern=r'\?.+$', repl='', string=parsed_request_url.path)).suffix or ''
                     ).removeprefix('.') or None
                 ),
                 fragment=parsed_request_url.fragment or None,
+                full=flow.request.url,
+                original=flow.request.url,
                 password=parsed_request_url.password,
-                username=parsed_request_url.username,
+                path=flow.request.path or None,
+                port=flow.request.port,
                 query=parsed_request_url.query or None,
+                registered_domain=domain_properties.registered_domain,
+                scheme=flow.request.scheme or None,
+                subdomain=domain_properties.subdomain,
+                top_level_domain=domain_properties.effective_top_level_domain,
+                username=parsed_request_url.username,
                 query_keys=[key for key, _ in query_key_value_pairs] or None,
                 query_values=[value for _, value in query_key_value_pairs] or None,
             ),
-            client=ClientLogEntry(
+            client=Client(
                 address=flow.client_conn.peername[0],
                 ip=flow.client_conn.peername[0],
                 port=flow.client_conn.peername[1]
             ),
-            server=ServerLogEntry(
+            server=Server(
                 address=flow.request.host,
                 ip=server_ip_address_str,
                 port=flow.server_conn.peername[1] if flow.server_conn.peername else None
             ),
-            network=NetworkLogEntry(
+            network=Network(
                 protocol=flow.request.scheme,
                 transport='tcp',
                 type=network_type
             ),
-            event=EventLogEntry(
+            user_agent=ecs_user_agent,
+            event=Event(
                 created=event_created,
-                timezone=re_sub(pattern=r'^(.{3})(.{2}).*$', repl=r'\1:\2', string=f'{event_created:%z}'),
+                timezone=event_timezone_from_datetime(dt=event_created),
                 start=datetime.fromtimestamp(flow.request.timestamp_start).astimezone(),
                 end=event_end
-
-            ),
-            user_agent=user_agent_log_entry
+            )
         )
 
     async def request(self, flow: HTTPFlow) -> None:
-        self._log_file.write(json_dumps(asdict(self._build_log_entry(flow=flow)), default=str))
+        self._log_file.write(json_dumps((self._build_log_entry(flow=flow).to_dict()), default=str) + '\n')
 
     async def response(self, flow: HTTPFlow) -> None:
-        self._log_file.write(json_dumps(asdict(self._build_log_entry(flow=flow)), default=str))
+        self._log_file.write(json_dumps((self._build_log_entry(flow=flow).to_dict()), default=str) + '\n')
 
 
 addons = [WebProxyLogger()]
