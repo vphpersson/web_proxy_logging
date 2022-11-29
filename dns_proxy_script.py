da144c8d1113c5a2902a5737b6cd7ca0f0e8d33d
Viktor Persson <vph.persson@gmail.com>
2022-11-30 00:11:44 +0100

    Use production code for DNS.

--- dns_proxy_script.py
+++ dns_proxy_script.py
@@ -1,35 +1,32 @@
-# from vyos.configquery import ConfigTreeQuery
-# from vyos.firewall import nft_update_set_elements
-
 from collections import defaultdict
 from asyncio import create_task
-from datetime import datetime
 
 from mitmproxy.dns import DNSFlow
 from mitmproxy.net.dns.types import A as DNS_A, AAAA as DNS_AAAA
 from inotify.adapters import Inotify
+from inotify.constants import IN_CLOSE_WRITE
+from vyos.configquery import ConfigTreeQuery
+from vyos.firewall import nft_update_set_elements
 
 
 def get_updated_domain_map() -> dict[str, set[str]]:
 
-    # config = ConfigTreeQuery()
-    # domain_group_name_to_domains: dict[str, list[str]] = {
-    #     domain_group_name: domain_group_config['address']
-    #     for domain_group_name, domain_group_config in config.get_config_dict(
-    #         path=['firewall', 'group', 'domain-group'],
-    #         key_mangling=('-', '_'),
-    #         get_first_key=True
-    #     ).items()
-    # }
+    config = ConfigTreeQuery()
+    domain_group_name_to_domains: dict[str, list[str]] = {
+        domain_group_name: domain_group_config['address']
+        for domain_group_name, domain_group_config in config.get_config_dict(
+            path=['firewall', 'group', 'domain-group'],
+            key_mangling=('-', '_'),
+            get_first_key=True
+        ).items()
+    }
 
     domain_to_group_names: defaultdict[str, set[str]] = defaultdict(set)
 
-    domain_to_group_names[datetime.now().isoformat()] = {'are', 'dog'}
-    #
-    # for domain_group_name , domains in domain_group_name_to_domains:
-    #     for domain in domains:
-    #         domain_to_group_names[domain].add(domain_group_name)
-    #
+    for domain_group_name , domains in domain_group_name_to_domains:
+        for domain in domains:
+            domain_to_group_names[domain].add(domain_group_name)
+
     return dict(domain_to_group_names)
 
 
@@ -40,28 +37,13 @@ class DNSProxy:
 
     async def _monitor_config_changes(self):
         i = Inotify()
-        i.add_watch(path_unicode='')
+        i.add_watch(path_unicode='/opt/vyatta/etc/config/archive/commits', mask=IN_CLOSE_WRITE)
 
-        for event in i.event_gen(yield_nones=False):
-            _, _, _, filename = event
-            print('bong', filename)
+        for _ in i.event_gen(yield_nones=False):
             self._domain_to_group_names = get_updated_domain_map()
 
     async def running(self):
-
         create_task(self._monitor_config_changes())
-
-        # def update_domain_map():
-        #     print(datetime.now().isoformat())
-        #     self._domain_to_group_names = get_updated_domain_map()
-        #     print(self._domain_to_group_names)
-        #
-        #     get_event_loop().call_later(
-        #         delay=10,
-        #         callback=update_domain_map
-        #     )
-        #
-        # update_domain_map()
 
     async def dns_response(self, flow: DNSFlow) -> None:
 
@@ -69,7 +51,6 @@ class DNSProxy:
 
         for answer in flow.response.answers:
             answer_name: str = answer.name
-            print(answer_name)
 
             if answer_name not in self._domain_to_group_names:
                 continue
@@ -83,8 +64,7 @@ class DNSProxy:
 
         for domain, ip_addresses in domain_to_ip_addresses.items():
             for group_name in self._domain_to_group_names[domain]:
-                # nft_update_set_elements(group_name=group_name, elements=ip_addresses)
-                pass
+                nft_update_set_elements(group_name=group_name, elements=ip_addresses)
 
 
 addons = [DNSProxy()]
@@ -105,4 +85,3 @@ addons = [DNSProxy()]
 #         nft_update_set_elements(group_name=domain_group_name, elements=ip_addresses)
 #
 # time.sleep(timeout)
-
